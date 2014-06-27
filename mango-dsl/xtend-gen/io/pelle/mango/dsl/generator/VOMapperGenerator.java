48d11e9bb4019d17d0de426a8ff1871d155f8a0e
Pelle Pelster <pelle@pelle.io>
2014-06-27 18:00:00 +0100

    added xtend generation

--- mango-dsl/xtend-gen/io/pelle/mango/dsl/generator/VOMapperGenerator.java
+++ mango-dsl/xtend-gen/io/pelle/mango/dsl/generator/VOMapperGenerator.java
@@ -1,119 +1,24 @@
 package io.pelle.mango.dsl.generator;
 
-import com.google.common.collect.Iterables;
 import com.google.inject.Inject;
-import io.pelle.mango.client.base.vo.IEntityVOMapper;
-import io.pelle.mango.dsl.generator.client.ClientNameUtils;
-import io.pelle.mango.dsl.generator.util.NameUtils;
-import io.pelle.mango.dsl.mango.Entity;
 import io.pelle.mango.dsl.mango.Model;
-import io.pelle.mango.server.base.BaseEntityVOMapper;
-import org.eclipse.emf.common.util.TreeIterator;
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.xtend2.lib.StringConcatenation;
 import org.eclipse.xtext.xbase.lib.Extension;
-import org.eclipse.xtext.xbase.lib.IteratorExtensions;
 
 @SuppressWarnings("all")
 public class VOMapperGenerator {
   @Inject
   @Extension
-  private NameUtils _nameUtils;
+  private /* NameUtils */Object _nameUtils;
   
-  private ClientNameUtils clientNameUtils = new ClientNameUtils();
+  private /* ClientNameUtils */Object clientNameUtils /* Skipped initializer because of errors */;
   
   public CharSequence compileVOMapper(final Model model) {
-    StringConcatenation _builder = new StringConcatenation();
-    _builder.append("package ");
-    String _modelPackageName = this._nameUtils.modelPackageName(model);
-    _builder.append(_modelPackageName, "");
-    _builder.append(";");
-    _builder.newLineIfNotEmpty();
-    _builder.append("\t");
-    _builder.newLine();
-    _builder.append("import java.util.HashMap;");
-    _builder.newLine();
-    _builder.append("import java.util.Map;");
-    _builder.newLine();
-    _builder.newLine();
-    _builder.append("@org.springframework.stereotype.Component");
-    _builder.newLine();
-    _builder.append("public class ");
-    String _voMapperName = this._nameUtils.voMapperName(model);
-    _builder.append(_voMapperName, "");
-    _builder.append(" extends ");
-    String _name = BaseEntityVOMapper.class.getName();
-    _builder.append(_name, "");
-    _builder.append(" implements ");
-    String _name_1 = IEntityVOMapper.class.getName();
-    _builder.append(_name_1, "");
-    _builder.append(" {");
-    _builder.newLineIfNotEmpty();
-    _builder.append("\t\t\t\t");
-    _builder.newLine();
-    _builder.append("\t");
-    _builder.append("@SuppressWarnings(\"serial\")");
-    _builder.newLine();
-    _builder.append("\t");
-    _builder.append("private Map<Class<?>, Class<?>> entityVOMapper = new HashMap<Class<?>, Class<?>>() {");
-    _builder.newLine();
-    _builder.append("\t\t");
-    _builder.append("{");
-    _builder.newLine();
-    {
-      TreeIterator<EObject> _eAllContents = model.eAllContents();
-      Iterable<EObject> _iterable = IteratorExtensions.<EObject>toIterable(_eAllContents);
-      Iterable<Entity> _filter = Iterables.<Entity>filter(_iterable, Entity.class);
-      for(final Entity entity : _filter) {
-        _builder.append("\t\t\t");
-        _builder.append("put(");
-        String _voFullQualifiedName = this.clientNameUtils.voFullQualifiedName(entity);
-        _builder.append(_voFullQualifiedName, "\t\t\t");
-        _builder.append(".class, ");
-        String _entityFullQualifiedName = this._nameUtils.entityFullQualifiedName(entity);
-        _builder.append(_entityFullQualifiedName, "\t\t\t");
-        _builder.append(".class);");
-        _builder.newLineIfNotEmpty();
-        _builder.append("\t\t\t");
-        _builder.append("put(");
-        String _entityFullQualifiedName_1 = this._nameUtils.entityFullQualifiedName(entity);
-        _builder.append(_entityFullQualifiedName_1, "\t\t\t");
-        _builder.append(".class, ");
-        String _voFullQualifiedName_1 = this.clientNameUtils.voFullQualifiedName(entity);
-        _builder.append(_voFullQualifiedName_1, "\t\t\t");
-        _builder.append(".class);");
-        _builder.newLineIfNotEmpty();
-      }
-    }
-    _builder.append("\t\t");
-    _builder.append("}");
-    _builder.newLine();
-    _builder.append("\t");
-    _builder.append("};");
-    _builder.newLine();
-    _builder.newLine();
-    _builder.append("\t");
-    _builder.append("protected Map<Class<?>, Class<?>> getClassMappings() {");
-    _builder.newLine();
-    _builder.append("\t\t");
-    _builder.append("return entityVOMapper;");
-    _builder.newLine();
-    _builder.append("\t");
-    _builder.append("}");
-    _builder.newLine();
-    _builder.newLine();
-    _builder.append("\t");
-    _builder.append("public Class<?> getMappedClass(Class<?> clazz) {");
-    _builder.newLine();
-    _builder.append("\t\t");
-    _builder.append("return entityVOMapper.get(clazz);");
-    _builder.newLine();
-    _builder.append("\t");
-    _builder.append("}");
-    _builder.newLine();
-    _builder.newLine();
-    _builder.append("}");
-    _builder.newLine();
-    return _builder;
+    throw new Error("Unresolved compilation problems:"
+      + "\nThe method modelPackageName is undefined for the type VOMapperGenerator"
+      + "\nThe method voMapperName is undefined for the type VOMapperGenerator"
+      + "\nThe method entityFullQualifiedName is undefined for the type VOMapperGenerator"
+      + "\nThe method entityFullQualifiedName is undefined for the type VOMapperGenerator"
+      + "\nvoFullQualifiedName cannot be resolved"
+      + "\nvoFullQualifiedName cannot be resolved");
   }
 }
--- mango-dsl/xtend-gen/io/pelle/mango/dsl/generator/client/ModuleGenerator.java
+++ mango-dsl/xtend-gen/io/pelle/mango/dsl/generator/client/ModuleGenerator.java
@@ -1,301 +1,32 @@
 package io.pelle.mango.dsl.generator.client;
 
-import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.inject.Inject;
-import io.pelle.mango.client.base.module.BaseModule;
-import io.pelle.mango.client.base.module.IModule;
-import io.pelle.mango.dsl.generator.client.ClientNameUtils;
-import io.pelle.mango.dsl.generator.client.ClientTypeUtils;
 import io.pelle.mango.dsl.mango.ModuleDefinition;
-import io.pelle.mango.dsl.mango.ModuleDefinitionParameter;
-import io.pelle.mango.dsl.mango.SimpleTypes;
-import org.eclipse.emf.common.util.EList;
-import org.eclipse.xtend2.lib.StringConcatenation;
 import org.eclipse.xtext.xbase.lib.Extension;
-import org.eclipse.xtext.xbase.lib.StringExtensions;
 
 @SuppressWarnings("all")
 public class ModuleGenerator {
   @Inject
   @Extension
-  private ClientNameUtils _clientNameUtils;
+  private /* ClientNameUtils */Object _clientNameUtils;
   
   @Inject
   @Extension
-  private ClientTypeUtils _clientTypeUtils;
+  private /* ClientTypeUtils */Object _clientTypeUtils;
   
   public CharSequence compileBaseModuleDefinition(final ModuleDefinition moduleDefinition) {
-    StringConcatenation _builder = new StringConcatenation();
-    _builder.append("package ");
-    String _packageName = this._clientNameUtils.getPackageName(moduleDefinition);
-    _builder.append(_packageName, "");
-    _builder.append(";");
-    _builder.newLineIfNotEmpty();
-    _builder.newLine();
-    _builder.append("public abstract class ");
-    String _baseModuleDefinitionName = this._clientNameUtils.baseModuleDefinitionName(moduleDefinition);
-    _builder.append(_baseModuleDefinitionName, "");
-    _builder.append(" extends ");
-    String _name = BaseModule.class.getName();
-    _builder.append(_name, "");
-    _builder.append(" {");
-    _builder.newLineIfNotEmpty();
-    _builder.newLine();
-    _builder.append("\t");
-    _builder.append("public static final String MODULE_ID = \"");
-    String _name_1 = moduleDefinition.getName();
-    _builder.append(_name_1, "\t");
-    _builder.append("\";");
-    _builder.newLineIfNotEmpty();
-    _builder.newLine();
-    _builder.append("\t");
-    _builder.append("public ");
-    String _baseModuleDefinitionName_1 = this._clientNameUtils.baseModuleDefinitionName(moduleDefinition);
-    _builder.append(_baseModuleDefinitionName_1, "\t");
-    _builder.append("(String moduleUrl, ");
-    String _name_2 = AsyncCallback.class.getName();
-    _builder.append(_name_2, "\t");
-    _builder.append("<");
-    String _name_3 = IModule.class.getName();
-    _builder.append(_name_3, "\t");
-    _builder.append("> moduleCallback, java.util.Map<String, Object> parameters) {");
-    _builder.newLineIfNotEmpty();
-    _builder.append("\t\t");
-    _builder.append("super(moduleUrl, moduleCallback, parameters);");
-    _builder.newLine();
-    _builder.append("\t");
-    _builder.append("}");
-    _builder.newLine();
-    _builder.newLine();
-    {
-      EList<ModuleDefinitionParameter> _moduleDefinitionParameters = moduleDefinition.getModuleDefinitionParameters();
-      for(final ModuleDefinitionParameter moduleDefinitionParameter : _moduleDefinitionParameters) {
-        _builder.append("\t");
-        _builder.newLine();
-        _builder.append("\t");
-        _builder.append("public static final String ");
-        String _name_4 = moduleDefinitionParameter.getName();
-        String _upperCase = _name_4.toUpperCase();
-        _builder.append(_upperCase, "\t");
-        _builder.append("_PARAMETER_ID = \"");
-        String _name_5 = moduleDefinitionParameter.getName();
-        _builder.append(_name_5, "\t");
-        _builder.append("\";");
-        _builder.newLineIfNotEmpty();
-        _builder.append("\t");
-        _builder.newLine();
-        _builder.append("\t");
-        _builder.append("public ");
-        SimpleTypes _type = moduleDefinitionParameter.getType();
-        _builder.append(_type, "\t");
-        _builder.append(" get");
-        String _name_6 = moduleDefinitionParameter.getName();
-        String _firstUpper = StringExtensions.toFirstUpper(_name_6);
-        _builder.append(_firstUpper, "\t");
-        _builder.append("() {");
-        _builder.newLineIfNotEmpty();
-        _builder.newLine();
-        _builder.append("\t");
-        _builder.append("\t");
-        _builder.append("if (getParameters().containsKey(\"");
-        String _name_7 = moduleDefinitionParameter.getName();
-        _builder.append(_name_7, "\t\t");
-        _builder.append("\"))");
-        _builder.newLineIfNotEmpty();
-        _builder.append("\t");
-        _builder.append("\t");
-        _builder.append("{");
-        _builder.newLine();
-        _builder.append("\t");
-        _builder.append("\t\t");
-        _builder.append("Object parameterValue = parameters.get(\"");
-        String _name_8 = moduleDefinitionParameter.getName();
-        _builder.append(_name_8, "\t\t\t");
-        _builder.append("\");");
-        _builder.newLineIfNotEmpty();
-        _builder.append("\t");
-        _builder.append("\t");
-        _builder.newLine();
-        _builder.append("\t");
-        _builder.append("\t\t");
-        _builder.append("if (parameterValue instanceof ");
-        SimpleTypes _type_1 = moduleDefinitionParameter.getType();
-        String _type_2 = this._clientTypeUtils.getType(_type_1);
-        _builder.append(_type_2, "\t\t\t");
-        _builder.append(")");
-        _builder.newLineIfNotEmpty();
-        _builder.append("\t");
-        _builder.append("\t\t");
-        _builder.append("{");
-        _builder.newLine();
-        _builder.append("\t");
-        _builder.append("\t\t\t");
-        _builder.append("return (");
-        SimpleTypes _type_3 = moduleDefinitionParameter.getType();
-        String _type_4 = this._clientTypeUtils.getType(_type_3);
-        _builder.append(_type_4, "\t\t\t\t");
-        _builder.append(") parameterValue;");
-        _builder.newLineIfNotEmpty();
-        _builder.append("\t");
-        _builder.append("\t\t");
-        _builder.append("}");
-        _builder.newLine();
-        _builder.append("\t");
-        _builder.append("\t\t");
-        _builder.newLine();
-        _builder.append("\t");
-        _builder.append("\t\t");
-        _builder.newLine();
-        _builder.append("\t");
-        _builder.append("\t\t");
-        _builder.append("if (parameterValue instanceof java.lang.String)");
-        _builder.newLine();
-        _builder.append("\t");
-        _builder.append("\t\t");
-        _builder.append("{");
-        _builder.newLine();
-        _builder.append("\t");
-        _builder.append("\t\t\t");
-        _builder.append("return ");
-        SimpleTypes _type_5 = moduleDefinitionParameter.getType();
-        String _parseSimpleTypeFromString = this._clientTypeUtils.parseSimpleTypeFromString(_type_5, "parameterValue.toString()");
-        _builder.append(_parseSimpleTypeFromString, "\t\t\t\t");
-        _builder.append(";");
-        _builder.newLineIfNotEmpty();
-        _builder.append("\t");
-        _builder.append("\t\t");
-        _builder.append("}");
-        _builder.newLine();
-        _builder.append("\t");
-        _builder.append("\t\t");
-        _builder.newLine();
-        _builder.append("\t");
-        _builder.append("\t\t");
-        _builder.append("throw new RuntimeException(\"parameter value type mismatch, expected \'");
-        SimpleTypes _type_6 = moduleDefinitionParameter.getType();
-        String _type_7 = this._clientTypeUtils.getType(_type_6);
-        _builder.append(_type_7, "\t\t\t");
-        _builder.append("\' but got \'\" + parameterValue.getClass().getName() + \"\'\");");
-        _builder.newLineIfNotEmpty();
-        _builder.append("\t");
-        _builder.append("\t");
-        _builder.append("}");
-        _builder.newLine();
-        _builder.append("\t");
-        _builder.append("\t");
-        _builder.append("else");
-        _builder.newLine();
-        _builder.append("\t");
-        _builder.append("\t");
-        _builder.append("{");
-        _builder.newLine();
-        _builder.append("\t");
-        _builder.append("\t\t");
-        _builder.append("return null;");
-        _builder.newLine();
-        _builder.append("\t");
-        _builder.append("\t");
-        _builder.append("}");
-        _builder.newLine();
-        _builder.append("\t");
-        _builder.append("}");
-        _builder.newLine();
-        _builder.append("\t");
-        _builder.newLine();
-        _builder.append("\t");
-        _builder.append("public boolean has");
-        String _name_9 = moduleDefinitionParameter.getName();
-        String _firstUpper_1 = StringExtensions.toFirstUpper(_name_9);
-        _builder.append(_firstUpper_1, "\t");
-        _builder.append("() {");
-        _builder.newLineIfNotEmpty();
-        _builder.append("\t");
-        _builder.append("\t");
-        _builder.append("return getParameters().containsKey(\"");
-        String _name_10 = moduleDefinitionParameter.getName();
-        _builder.append(_name_10, "\t\t");
-        _builder.append("\");");
-        _builder.newLineIfNotEmpty();
-        _builder.append("\t");
-        _builder.append("}");
-        _builder.newLine();
-        _builder.newLine();
-      }
-    }
-    _builder.newLine();
-    _builder.append("\t");
-    _builder.append("public static java.util.Map<String, Object> getParameterMap(");
-    CharSequence _moduleDefinitionParameters_1 = this.moduleDefinitionParameters(moduleDefinition);
-    _builder.append(_moduleDefinitionParameters_1, "\t");
-    _builder.append(") {");
-    _builder.newLineIfNotEmpty();
-    _builder.append("\t");
-    _builder.newLine();
-    _builder.append("\t\t");
-    _builder.append("java.util.Map<String, Object> parameterMap = new java.util.HashMap<String, Object>();");
-    _builder.newLine();
-    _builder.newLine();
-    {
-      EList<ModuleDefinitionParameter> _moduleDefinitionParameters_2 = moduleDefinition.getModuleDefinitionParameters();
-      for(final ModuleDefinitionParameter moduleDefinitionParameter_1 : _moduleDefinitionParameters_2) {
-        _builder.append("\t\t");
-        _builder.append("parameterMap.put(");
-        String _name_11 = moduleDefinitionParameter_1.getName();
-        String _upperCase_1 = _name_11.toUpperCase();
-        _builder.append(_upperCase_1, "\t\t");
-        _builder.append("_PARAMETER_ID, ");
-        String _name_12 = moduleDefinitionParameter_1.getName();
-        String _firstLower = StringExtensions.toFirstLower(_name_12);
-        _builder.append(_firstLower, "\t\t");
-        _builder.append(");");
-        _builder.newLineIfNotEmpty();
-      }
-    }
-    _builder.append("\t\t");
-    _builder.newLine();
-    _builder.append("\t\t");
-    _builder.append("return parameterMap;");
-    _builder.newLine();
-    _builder.append("\t");
-    _builder.append("}");
-    _builder.newLine();
-    _builder.newLine();
-    _builder.append("\t");
-    _builder.append("public boolean hasParameter(String parameterName) {");
-    _builder.newLine();
-    _builder.append("\t\t");
-    _builder.append("return getParameters().containsKey(parameterName);");
-    _builder.newLine();
-    _builder.append("\t");
-    _builder.append("}");
-    _builder.newLine();
-    _builder.newLine();
-    _builder.append("}");
-    _builder.newLine();
-    return _builder;
+    throw new Error("Unresolved compilation problems:"
+      + "\nThe method getPackageName is undefined for the type ModuleGenerator"
+      + "\nThe method baseModuleDefinitionName is undefined for the type ModuleGenerator"
+      + "\nThe method baseModuleDefinitionName is undefined for the type ModuleGenerator"
+      + "\nThe method getType is undefined for the type ModuleGenerator"
+      + "\nThe method getType is undefined for the type ModuleGenerator"
+      + "\nThe method parseSimpleTypeFromString is undefined for the type ModuleGenerator"
+      + "\nThe method getType is undefined for the type ModuleGenerator");
   }
   
   public CharSequence moduleDefinitionParameters(final ModuleDefinition moduleDefinition) {
-    StringConcatenation _builder = new StringConcatenation();
-    {
-      EList<ModuleDefinitionParameter> _moduleDefinitionParameters = moduleDefinition.getModuleDefinitionParameters();
-      boolean _hasElements = false;
-      for(final ModuleDefinitionParameter moduleDefinitionParameter : _moduleDefinitionParameters) {
-        if (!_hasElements) {
-          _hasElements = true;
-        } else {
-          _builder.appendImmediate(", ", "");
-        }
-        SimpleTypes _type = moduleDefinitionParameter.getType();
-        String _type_1 = this._clientTypeUtils.getType(_type);
-        _builder.append(_type_1, "");
-        _builder.append(" ");
-        String _name = moduleDefinitionParameter.getName();
-        String _firstLower = StringExtensions.toFirstLower(_name);
-        _builder.append(_firstLower, "");
-        _builder.newLineIfNotEmpty();
-      }
-    }
-    return _builder;
+    throw new Error("Unresolved compilation problems:"
+      + "\nThe method getType is undefined for the type ModuleGenerator");
   }
 }
