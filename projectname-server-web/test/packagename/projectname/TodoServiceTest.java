ced6e4e7a1d185d19e5c88d89a7b32ceabab52e1
Pelle Pelster <pelle@pelle.io>
2014-08-10 09:30:31 +0200

    fixed test classpath

--- projectname-server-web/test/packagename/projectname/TodoServiceTest.java
+++ projectname-server-web/test/packagename/projectname/TodoServiceTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 
-import packagename.TodoVO;
+import packagename.client.TodoVO;
 
 @ContextConfiguration(locations = { "classpath:/ProjectNameApplicationContext.xml", "classpath:/ProjectNameDB-gen.xml", "classpath:/ProjectNameBaseApplicationContext-gen.xml" })
 public class TodoServiceTest extends BaseDBTest {
