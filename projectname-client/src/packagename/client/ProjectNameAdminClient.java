e66d4c1cfa2ec04990a89052038355948cf76d02
pelle <pelle@pelle.io>
2014-08-11 17:33:05 +0200

    added admin client

---
+++ projectname-client/src/packagename/client/ProjectNameAdminClient.java
@@ -0,0 +1,33 @@
+package packagename.client;
+
+import io.pelle.mango.client.gwt.GWTLayoutFactory;
+
+import io.pelle.mango.client.web.MangoClientWeb;
+import io.pelle.mango.client.web.module.ModuleHandler;
+import io.pelle.mango.client.web.modules.navigation.ModuleNavigationModule;
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.shared.GWT;
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.user.client.ui.DockLayoutPanel.Direction;
+import packagename.ProjectNameClientConfiguration;
+
+public class ProjectNameAdminClient implements EntryPoint {
+	private static final ProjectNameResources RESOURCES = GWT
+			.create(ProjectNameResources.class);
+
+	/** {@inheritDoc} */
+	@Override
+	public void onModuleLoad() {
+		
+		GWTLayoutFactory gwtLayoutFactory = new GWTLayoutFactory(Unit.PX);
+		MangoClientWeb.getInstance().setLayoutFactory(gwtLayoutFactory);
+		
+		init();
+		
+		ModuleHandler.getInstance().startUIModule(ModuleNavigationModule.NAVIGATION_UI_MODULE_LOCATOR, Direction.WEST.toString());
+	}
+
+	public void init() {
+		ProjectNameClientConfiguration.registerAll();
+	}
+}
---
+++ projectname-client/src/packagename/client/ProjectNameResources.java
@@ -0,0 +1,10 @@
+package packagename.client;
+
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.ImageResource;
+
+public interface ProjectNameResources extends ClientBundle
+{
+	@Source("todo.png")
+	ImageResource address();
+}
