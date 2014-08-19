d6d8b85ff7519e3e73a9806e5b1fc0ced44724dc
Pelle Pelster <pelle@pelle.io>
2014-08-19 19:35:11 +0200

    base cli project

---
+++ mango-demo-server/src/AccountServiceEndpoint.java
@@ -0,0 +1,17 @@
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+import org.w3c.dom.Element;
+
+@Endpoint
+public class AccountServiceEndpoint {
+	
+	private static final String TARGET_NAMESPACE = "http://mangodemo/accountservice";
+	
+	@PayloadRoot(localPart = "AccountDetailsRequest", namespace = TARGET_NAMESPACE)
+	public @ResponsePayload Element getAccountDetails(@RequestPayload Element element) {
+		return null;
+	}
+
+}
