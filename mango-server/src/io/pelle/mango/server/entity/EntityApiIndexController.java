79f0f18ab4ef7aab3b66fe7eddc6fef85905ee43
pelle <pelle@pelle.io>
2015-02-09 09:28:36 +0100

    api index template fixes

--- mango-server/src/io/pelle/mango/server/entity/EntityApiIndexController.java
+++ mango-server/src/io/pelle/mango/server/entity/EntityApiIndexController.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,16 +19,19 @@ public class EntityApiIndexController {
 
 	public final static String ENTITY_LABEL_TEMPLATE_VARIABLE = "entityLabel";
 
+	public final static String BASE_URL_TEMPLATE_VARIABLE = "baseUrl";
+
 	@Autowired
 	private VOMetaDataService voMetaDataService;
 
 	@RequestMapping(value = "entity/{entityName}/api/index", method = RequestMethod.GET)
-	public ModelAndView index(@PathVariable String entityName) {
+	public ModelAndView index(@PathVariable String entityName, HttpRequest httpRequest) {
 
 		Class<? extends IBaseEntity> entityClass = voMetaDataService.getEntityClassForName(entityName);
 
 		Map<String, String> templateVariables = new HashMap<String, String>();
 		templateVariables.put(ENTITY_LABEL_TEMPLATE_VARIABLE, voMetaDataService.getLabel(entityClass));
+		templateVariables.put(BASE_URL_TEMPLATE_VARIABLE, httpRequest.getURI().toString());
 
 		return new ModelAndView("EntityApiIndex", templateVariables);
 	}
