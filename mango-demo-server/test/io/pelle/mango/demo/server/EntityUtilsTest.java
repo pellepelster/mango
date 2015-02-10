c97154e2b686b09d500cf375cbb69f251d4c8ea3
pelle <pelle@pelle.io>
2015-02-10 09:22:35 +0100

    json export expression parsing

--- mango-demo-server/test/io/pelle/mango/demo/server/EntityUtilsTest.java
+++ mango-demo-server/test/io/pelle/mango/demo/server/EntityUtilsTest.java
@@ -1,6 +1,9 @@
 package io.pelle.mango.demo.server;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import io.pelle.mango.db.test.mockup.EntityVOMapper;
+import io.pelle.mango.demo.client.showcase.CountryVO;
 import io.pelle.mango.demo.server.showcase.Country;
 import io.pelle.mango.demo.server.showcase.Customer;
 import io.pelle.mango.server.entity.EntityUtils;
@@ -12,42 +15,68 @@ public class EntityUtilsTest extends BaseDemoTest {
 
 	@Test
 	public void testSimpleParseReferenceTree() {
-		
+
 		ReferenceTree referenceTree = EntityUtils.parseReferenceTree(Customer.class, "title,firstName,LastName");
 		assertEquals(3, referenceTree.getAttributePaths().size());
 		assertTrue(referenceTree.getAttributePaths().keySet().contains(Customer.TITLE));
 		assertTrue(referenceTree.getAttributePaths().keySet().contains(Customer.FIRSTNAME));
 		assertTrue(referenceTree.getAttributePaths().keySet().contains(Customer.LASTNAME));
-		
+
 		referenceTree = EntityUtils.parseReferenceTree(Customer.class, "titlE,firstName,LASTName");
 		assertEquals(3, referenceTree.getAttributePaths().size());
 		assertTrue(referenceTree.getAttributePaths().keySet().contains(Customer.TITLE));
 		assertTrue(referenceTree.getAttributePaths().keySet().contains(Customer.FIRSTNAME));
 		assertTrue(referenceTree.getAttributePaths().keySet().contains(Customer.LASTNAME));
 	}
-	
+
 	@Test
 	public void testDeepParseReferenceTree() {
-		
+
 		ReferenceTree referenceTree = EntityUtils.parseReferenceTree(Customer.class, "title,firstName,country[countryname]");
 		assertEquals(3, referenceTree.getAttributePaths().size());
 		assertTrue(referenceTree.getAttributePaths().keySet().contains(Customer.TITLE));
 		assertTrue(referenceTree.getAttributePaths().keySet().contains(Customer.FIRSTNAME));
 		assertTrue(referenceTree.getAttributePaths().keySet().contains(Customer.COUNTRY));
-		
+
 		referenceTree = referenceTree.getAttributePaths().get(Customer.COUNTRY);
 		assertEquals(1, referenceTree.getAttributePaths().size());
 		assertTrue(referenceTree.getAttributePaths().keySet().contains(Country.COUNTRYNAME));
 	}
 
-	@Test(expected=RuntimeException.class)
+	@Test(expected = RuntimeException.class)
 	public void testInvalidAttributePath() {
 		EntityUtils.parseReferenceTree(Customer.class, "aaa");
 	}
-	
-	@Test(expected=RuntimeException.class)
+
+	@Test(expected = RuntimeException.class)
 	public void testInvalidAttributePathNonVOType() {
 		EntityUtils.parseReferenceTree(Customer.class, "title[name]");
 	}
-	
+
+	@Test(expected = RuntimeException.class)
+	public void testCreateInvalidExpression() {
+		assertEquals("xxx", EntityUtils.createSelectQuery(CountryVO.class, "name == 'xxx'").getJPQL(EntityVOMapper.INSTANCE));
+	}
+
+	@Test
+	public void testCreateSimpleExpression() {
+		assertEquals("SELECT x0 FROM CountryVO x0 WHERE x0.countryName = 'xxx'", EntityUtils.createSelectQuery(CountryVO.class, "countryName == 'xxx'").getJPQL(EntityVOMapper.INSTANCE));
+	}
+
+	@Test
+	public void testCreateAndExpression() {
+		assertEquals("SELECT x0 FROM CountryVO x0 WHERE (x0.countryName = 'xxx' AND x0.countryName = 'yyy')", EntityUtils.createSelectQuery(CountryVO.class, "countryName == 'xxx' && countryName == 'yyy'").getJPQL(EntityVOMapper.INSTANCE));
+	}
+
+	@Test
+	public void testCreateExpressionBrackets() {
+		assertEquals("SELECT x0 FROM CountryVO x0 WHERE (x0.countryName = 'xxx' OR (x0.countryName = 'zzz' AND x0.countryName = 'uuu') )",
+				EntityUtils.createSelectQuery(CountryVO.class, "countryName == 'xxx' || (countryName == 'zzz' && countryName == 'uuu')").getJPQL(EntityVOMapper.INSTANCE));
+	}
+
+	@Test
+	public void testCreateOrExpression() {
+		assertEquals("SELECT x0 FROM CountryVO x0 WHERE (x0.countryName = 'xxx' OR x0.countryName = 'yyy')", EntityUtils.createSelectQuery(CountryVO.class, "countryName == 'xxx' || countryName == 'yyy'").getJPQL(EntityVOMapper.INSTANCE));
+	}
+
 }
--- mango-server/src/io/pelle/mango/server/entity/EntityUtils.java
+++ mango-server/src/io/pelle/mango/server/entity/EntityUtils.java
@@ -1,13 +1,34 @@
 package io.pelle.mango.server.entity;
 
 import io.pelle.mango.client.base.vo.IAttributeDescriptor;
+import io.pelle.mango.client.base.vo.IBaseVO;
 import io.pelle.mango.client.base.vo.IVOEntity;
+import io.pelle.mango.client.base.vo.query.ComparisonOperator;
+import io.pelle.mango.client.base.vo.query.IBooleanExpression;
+import io.pelle.mango.client.base.vo.query.IExpression;
+import io.pelle.mango.client.base.vo.query.LOGICAL_OPERATOR;
+import io.pelle.mango.client.base.vo.query.SelectQuery;
+import io.pelle.mango.client.base.vo.query.expressions.BooleanExpression;
+import io.pelle.mango.client.base.vo.query.expressions.CompareExpression;
+import io.pelle.mango.client.base.vo.query.expressions.PathExpression;
+import io.pelle.mango.client.base.vo.query.expressions.StringExpression;
+import io.pelle.mango.db.query.ServerSelectQuery;
 import io.pelle.mango.db.voquery.AttributesDescriptorQuery;
 import io.pelle.mango.db.voquery.VOClassQuery;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
+
+import org.springframework.expression.spel.SpelNode;
+import org.springframework.expression.spel.ast.IntLiteral;
+import org.springframework.expression.spel.ast.OpAnd;
+import org.springframework.expression.spel.ast.OpEQ;
+import org.springframework.expression.spel.ast.OpOr;
+import org.springframework.expression.spel.ast.PropertyOrFieldReference;
+import org.springframework.expression.spel.ast.StringLiteral;
+import org.springframework.expression.spel.standard.SpelExpression;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
 
 public class EntityUtils {
 
@@ -68,4 +89,65 @@ public class EntityUtils {
 		return result;
 	}
 
+	public static <T extends IBaseVO> ServerSelectQuery<T> createSelectQuery(Class<T> voClass, String expressionString) {
+
+		IExpression expression = createExpression(voClass, expressionString);
+
+		SelectQuery<T> selectQuery = SelectQuery.selectFrom(voClass).where((IBooleanExpression) expression);
+
+		return ServerSelectQuery.adapt(selectQuery);
+	}
+
+	public static IExpression createExpression(Class<? extends IBaseVO> voClass, String expressionString) {
+
+		SpelExpressionParser parser = new SpelExpressionParser();
+		SpelExpression exp = parser.parseRaw(expressionString);
+		IExpression expression = createExpression(exp.getAST(), voClass);
+		return expression;
+	}
+
+	public static IExpression createExpression(SpelNode spelNode, Class<? extends IBaseVO> voClass) {
+
+		VOClassQuery voClassQuery = VOClassQuery.createQuery(voClass);
+
+		if (spelNode instanceof OpAnd) {
+			OpAnd node = (OpAnd) spelNode;
+			return new BooleanExpression(createExpression(node.getLeftOperand(), voClass), LOGICAL_OPERATOR.AND, createExpression(node.getRightOperand(), voClass));
+		}
+
+		if (spelNode instanceof OpOr) {
+			OpOr node = (OpOr) spelNode;
+			return new BooleanExpression(createExpression(node.getLeftOperand(), voClass), LOGICAL_OPERATOR.OR, createExpression(node.getRightOperand(), voClass));
+		}
+
+		if (spelNode instanceof OpEQ) {
+			OpEQ node = (OpEQ) spelNode;
+
+			return new CompareExpression(createExpression(node.getLeftOperand(), voClass), ComparisonOperator.EQUALS, createExpression(node.getRightOperand(), voClass));
+		}
+
+		if (spelNode instanceof PropertyOrFieldReference) {
+			PropertyOrFieldReference node = (PropertyOrFieldReference) spelNode;
+
+			String attributeName = node.getName();
+
+			if (!voClassQuery.attributesDescriptors().byName(attributeName).hasExactlyOne()) {
+				throw new RuntimeException(String.format("attribute '%s' not found for class '%s'", attributeName, voClass.getName()));
+			}
+
+			return new PathExpression(voClass.getName(), node.getName());
+		}
+
+		if (spelNode instanceof IntLiteral) {
+			IntLiteral node = (IntLiteral) spelNode;
+			return new StringExpression(node.getOriginalValue());
+		}
+
+		if (spelNode instanceof StringLiteral) {
+			StringLiteral node = (StringLiteral) spelNode;
+			return new StringExpression(node.getLiteralValue().getValue().toString());
+		}
+
+		throw new RuntimeException(String.format("unsupported spel node '%s'", spelNode));
+	}
 }
