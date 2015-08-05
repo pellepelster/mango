60d0c3edf9e847566767212bc27a218895caf995
Pelle Pelster <pelle@pelle.io>
2015-08-05 22:34:00 +0200

    new build script for ci builds

--- fullBuild.sh
+++ fullBuild.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-$DIR/gradlew --build-file mango-gradle-plugin/build.gradle uploadLocalDevelopment
-$DIR/gradlew --build-file mango-build/build.gradle clean build uploadLocalDevelopment
+if [ -z "$BUILD_NUMBER" ]; then
+    echo "BUILD_NUMBER not set"
+    exit 1
+fi
 
-rm -rf $DIR/project1
-cat $DIR/bootstrap.sh | bash -s org.example.Project1
+GRADLE_OPTIONS=""
 
-$DIR/gradlew --refresh-dependencies --build-file ./project1/project1-build/build.gradle build jettyEclipseRun
+if [ "$GRADLE_DEBUG" = true ]; then
+	GRADLE_OPTIONS="${GRADLE_OPTIONS} --debug"
+fi
+
+if [ "$GRADLE_RERUN_TASKS" = true ]; then
+	GRADLE_OPTIONS="${GRADLE_OPTIONS} --rerun-tasks"
+fi
+
+echo "gradle options: ${GRADLE_OPTIONS}"
+
+VERSION_QUALIFIER=${BUILD_NUMBER}
+ 
+GRADLE_OPTS="-Xmx1400m -XX:MaxPermSize=256M" $DIR/gradlew $GRADLE_OPTIONS --build-file=mango-build/build.gradle -PversionQualifier=$VERSION_QUALIFIER clean build uploadReleaseBuild
+GRADLE_OPTS="-Xmx1024m -XX:MaxPermSize=256M" $DIR/gradlew $GRADLE_OPTIONS --build-file=mango-gradle-plugin/build.gradle -PversionQualifier=$VERSION_QUALIFIER clean build uploadReleaseBuild
