47d8f864d2851c1243620486c268b86cf54ef20c
Pelle Pelster <pelle@pelle.io>
2015-08-05 20:42:17 +0200

    release test 1

--- uploadLocalDevelopmentFast.sh
+++ uploadLocalDevelopmentFast.sh
@@ -3,4 +3,3 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 $DIR/gradlew --build-file mango-gradle-plugin/build.gradle clean uploadLocalDevelopment
 $DIR/gradlew --build-file mango-build/build.gradle -PskipCompileGwt=true -PskipPdeBuild=true clean uploadLocalDevelopment
-
