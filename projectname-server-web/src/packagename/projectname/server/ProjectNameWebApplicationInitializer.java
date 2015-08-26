e2800568336e14868ec3d9ae14474c0e5a304d1a
Pelle Pelster <pelle@pelle.io>
2015-08-26 23:15:54 +0200

    migrated web xml to application initializer

---
+++ projectname-server-web/src/packagename/projectname/server/ProjectNameWebApplicationInitializer.java
@@ -0,0 +1,11 @@
+package packagename.projectname.server;
+
+import io.pelle.mango.server.MangoWebApplicationInitializer;
+
+public class ProjectNameWebApplicationInitializer extends MangoWebApplicationInitializer {
+
+	public ProjectNameWebApplicationInitializer() {
+		super(ProjectNameWebApplicationContext.class);
+	}
+
+}
