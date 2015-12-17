f8f8305059698ded260ac75e901de16640c21e47
Pelle Pelster <pelle@pelle.io>
2015-12-17 23:09:02 +0100

    removed server-base dep from dsl project

--- mango-dsl/src/io/pelle/mango/dsl/query/BaseEObjectCollectionQuery.java
+++ mango-dsl/src/io/pelle/mango/dsl/query/BaseEObjectCollectionQuery.java
@@ -1,10 +1,5 @@
 package io.pelle.mango.dsl.query;
 
-import io.pelle.mango.dsl.query.functions.FunctionEReferenceTypeSelect;
-import io.pelle.mango.dsl.query.functions.FunctionStructuralFeatureTypeSelect;
-import io.pelle.mango.dsl.query.predicates.EObjectStructuralFeatureEqualsPredicate;
-import io.pelle.mango.server.base.query.BaseCollectionQuery;
-
 import java.util.Collection;
 
 import org.eclipse.emf.ecore.EObject;
@@ -14,42 +9,37 @@ import org.eclipse.emf.ecore.EStructuralFeature;
 import com.google.common.base.Predicates;
 import com.google.common.collect.Collections2;
 
-public class BaseEObjectCollectionQuery<T extends EObject> extends BaseCollectionQuery<T>
-{
-	public BaseEObjectCollectionQuery(Collection<T> eObjects)
-	{
+import io.pelle.mango.dsl.query.functions.FunctionEReferenceTypeSelect;
+import io.pelle.mango.dsl.query.functions.FunctionStructuralFeatureTypeSelect;
+import io.pelle.mango.dsl.query.predicates.EObjectStructuralFeatureEqualsPredicate;
+
+public class BaseEObjectCollectionQuery<T extends EObject> extends BaseCollectionQuery<T> {
+	public BaseEObjectCollectionQuery(Collection<T> eObjects) {
 		super(eObjects);
 	}
 
-	protected <K extends EObject> Collection<K> transform(EStructuralFeature eStructuralFeature, Class<K> typeClass)
-	{
+	protected <K extends EObject> Collection<K> transform(EStructuralFeature eStructuralFeature, Class<K> typeClass) {
 		Collection<K> result = Collections2.transform(getCollection(), FunctionStructuralFeatureTypeSelect.create(eStructuralFeature, typeClass));
 
 		return Collections2.filter(result, Predicates.notNull());
 	}
 
-	protected <K extends EObject> Collection<K> transform(EReference eReference, Class<K> typeClass)
-	{
+	protected <K extends EObject> Collection<K> transform(EReference eReference, Class<K> typeClass) {
 		Collection<K> result = Collections2.transform(getCollection(), FunctionEReferenceTypeSelect.create(eReference, typeClass));
 
 		return Collections2.filter(result, Predicates.notNull());
 	}
 
-	protected Collection<T> getByEStructuralFeature(EStructuralFeature eStructuralFeature, Object object)
-	{
+	protected Collection<T> getByEStructuralFeature(EStructuralFeature eStructuralFeature, Object object) {
 		return Collections2.filter(getCollection(), EObjectStructuralFeatureEqualsPredicate.create(eStructuralFeature, object));
 	}
 
-	protected T getSingleByEStructuralFeature(EStructuralFeature eStructuralFeature, Object object)
-	{
+	protected T getSingleByEStructuralFeature(EStructuralFeature eStructuralFeature, Object object) {
 		Collection<T> result = getByEStructuralFeature(eStructuralFeature, object);
 
-		if (result.size() != 1)
-		{
+		if (result.size() != 1) {
 			throw new RuntimeException(String.format("%d EObjects match '%s' for '%s'", result.size(), eStructuralFeature.getName(), object.toString()));
-		}
-		else
-		{
+		} else {
 			return result.iterator().next();
 		}
 	}
